611a023d2c737eafc236b977fd94c32484d54804
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-22 12:27:05 -0300

    feat: Draw the player

--- script.js
+++ script.js
@@ -8,25 +8,42 @@ window.addEventListener('load', function() {
     constructor() {
       this.keys = [];
       window.addEventListener('keydown', e => {
-        if ((e.key === 'ArrowDown'
-          || e.key === 'ArrowUp'
-          || e.key === 'ArrowLeft'
-          || e.key === 'ArrowRight')
-          && this.keys.indexOf(e.key) === -1) {
+        if ((e.key === 'ArrowDown' ||
+            e.key === 'ArrowUp' ||
+            e.key === 'ArrowLeft' ||
+            e.key === 'ArrowRight')
+            && this.keys.indexOf(e.key) === -1) {
           this.keys.push(e.key);
         }
-        console.log(e.key, this.keys);
       });
       window.addEventListener('keydown', e => {
-        if (e.key === 'ArrowDown') {
+        if (e.key === 'ArrowDown' ||
+            e.key === 'ArrowUp' ||
+            e.key === 'ArrowLeft' ||
+            e.key === 'ArrowRight') {
           this.keys.splice(this.keys.indexOf(e.key), 1);
         }
-        console.log(e.key, this.keys);
       });
     };
   };
 
-  class Player {};
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+    }
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+      this.x ++;
+    }
+  };
 
   class Background {};
 
@@ -37,6 +54,14 @@ window.addEventListener('load', function() {
   function displayStatusText() {};
 
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  player.update();
 
-  function animate() {};
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update();
+    requestAnimationFrame(animate);
+  };
+  animate();
 });
