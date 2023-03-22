dcac40915fef3ec6c7540fc1522caa2c816b7662
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-22 13:53:10 -0300

    feat: Add Ebemy Class

--- script.js
+++ script.js
@@ -54,7 +54,8 @@ window.addEventListener('load', function() {
         this.x,
         this.y,
         this.width,
-        this.height);
+        this.height
+      );
     }
     update(input) {
       if (input.keys.indexOf('ArrowRight') > -1) {
@@ -89,9 +90,67 @@ window.addEventListener('load', function() {
     }
   };
 
-  class Background {};
+  class Background {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('backgroundImage');
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 720;
+      this.speed = 5;
+    }
+    draw(context) {
+      context.drawImage(
+        this.image,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+      context.drawImage(
+        this.image,
+        this.x + this.width - this.speed,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    update() {
+      this.x -= this.speed
+      if (this.x < 0 - this.width) this.x = 0;
+    }
+  };
 
-  class Enemy {};
+  class Enemy {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('enemyImage');
+      this.width = 160;
+      this.height = 119;
+      this.x = this.gameWidth;
+      this.y = this.gameHeight - this.height;
+      this.frameX = 0;
+    }
+    draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      )
+    }
+    update() {
+      this.x --;
+    }
+  };
 
   function handleEnemies() {};
 
@@ -99,11 +158,17 @@ window.addEventListener('load', function() {
 
   const input = new InputHandler();
   const player = new Player(canvas.width, canvas.height);
+  const background = new Background(canvas.width, canvas.height);
+  const enemy1 = new Enemy(canvas.width, canvas.height);
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw(ctx);
+    // background.update();
     player.draw(ctx);
     player.update(input);
+    enemy1.draw(ctx);
+    enemy1.update();
     requestAnimationFrame(animate);
   };
   animate();
