e4577fc985febd7ebed1d77c8997b95a0643155c
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-22 22:43:47 -0300

    fix: Better the collision detection

--- script.js
+++ script.js
@@ -76,6 +76,17 @@ window.addEventListener('load', function() {
       this.frameY = 0;
     }
     draw(context) {
+      // context.lineWidth = 5;
+      // context.strokeStyle = 'white';
+      // context.beginPath();
+      // context.arc(
+      //   this.x + this.width/2,
+      //   this.y + this.height/2,
+      //   this.width/2,
+      //   this.vy,
+      //   Math.PI * 2
+      // );
+      // context.stroke();
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -91,10 +102,10 @@ window.addEventListener('load', function() {
     update(input, deltaTime, enemies) {
       // collision detection
       enemies.forEach(enemy => {
-        const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
-        const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
+        const dx = (enemy.x + enemy.width/2 - 20) - (this.x + this.width/2);
+        const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2 + 20);
         const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < enemy.width/2 + this.width/2) {
+        if (distance < enemy.width/3 + this.width/3) {
           gameOver = true;
         }
       })
@@ -289,7 +300,7 @@ window.addEventListener('load', function() {
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
-    // background.update();
+    background.update();
     player.draw(ctx);
     player.update(input, deltaTime, enemies);
     handleEnemies(deltaTime);
